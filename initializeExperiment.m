ec55061580a68a710885bfbcb4c23705b4c27699
kdg862 <kevin.d.gallagher2@gmail.com>
2021-12-09 14:47:09 -0600

    removed sex field from experiment meta data

--- initializeExperiment.m
+++ initializeExperiment.m
@@ -14,7 +14,6 @@ num_meas = length(Directory);
 % initialize some data storage structures
 filename = cell(num_meas,1);
 genotype = cell(num_meas,1);
-sex = cell(num_meas,1);
 
 % string parsing based on filename to record the genotype and sex of each
 % image
@@ -25,8 +24,7 @@ for t = 1:num_meas
     temp_namestr = strrep(temp_namestr,'_',' ');        % replace '_' with ' '
     filename{t} = temp_namestr;                         % record filename
     split_str = strsplit(temp_namestr);                 % parse out the genotype specifically
-    genotype{t} = split_str{2};                         % record genotype
-    sex{t} = split_str{1};                              % record sex
+    genotype{t} = split_str{1};                         % record genotype
     
 end
 
@@ -42,8 +40,6 @@ end
 % convert to string arrays
 filename = string(filename);
 genotype = string(genotype);
-% genotype_full = string(genotype_full);
-sex = string(sex);
 
 % make struct containing all the info we want
 field1 = 'experimentID';    value1 = {experimentID};
@@ -52,8 +48,7 @@ field3 = 'filepath_output'; value3 = {filepath_out};
 field4 = 'filenames';       value4 = {filename};
 field5 = 'genotypes_code';  value5 = {genotype};
 field6 = 'genotypes_full';  value6 = {genotype_full};
-field7 = 'sex';             value7 = {sex};
 
 % return struct containing all the info we want
 expInfo = struct(field1,value1,field2,value2,field3,value3,...
-    field4,value4,field5,value5,field6,value6,field7,value7);
+    field4,value4,field5,value5,field6,value6);
